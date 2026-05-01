ebca03739aa0c97a1c19234bedfe5bd1eb8b817b
Nana Adjoa <adjoaaustinn@gmail.com>
2026-05-01 19:21:20 +0000

    chore: update tsconfig module resolution settings and add type definitions for image assets

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,32 +1,29 @@
-/// <reference types="vite/client" />
-
-// Image asset declarations
-declare module '*.png' {
-  const src: string;
-  export default src;
+declare module "*.png" {
+  const value: string;
+  export default value;
 }
 
-declare module '*.jpg' {
-  const src: string;
-  export default src;
+declare module "*.jpg" {
+  const value: string;
+  export default value;
 }
 
-declare module '*.jpeg' {
-  const src: string;
-  export default src;
+declare module "*.jpeg" {
+  const value: string;
+  export default value;
 }
 
-declare module '*.gif' {
-  const src: string;
-  export default src;
+declare module "*.svg" {
+  const value: string;
+  export default value;
 }
 
-declare module '*.webp' {
-  const src: string;
-  export default src;
+declare module "*.gif" {
+  const value: string;
+  export default value;
 }
 
-declare module '*.svg' {
-  const src: string;
-  export default src;
+declare module "*.webp" {
+  const value: string;
+  export default value;
 }
